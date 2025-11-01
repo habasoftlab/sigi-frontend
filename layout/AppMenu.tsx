cc21f548889d09863023598609b6f1e2199ce638
IsaiasJA <isaiasjuareza04@gmail.com>
2025-11-01 11:22:46 -0600

    Limpieza de plantilla SAKAI. Closes #1

---
+++ layout/AppMenu.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-img-element */
+
+import React, { useContext } from 'react';
+import AppMenuitem from './AppMenuitem';
+import { LayoutContext } from './context/layoutcontext';
+import { MenuProvider } from './context/menucontext';
+import { AppMenuItem } from '@/types';
+
+const AppMenu = () => {
+    const { layoutConfig } = useContext(LayoutContext);
+
+    const model: AppMenuItem[] = [
+        {
+            label: 'Inicio',
+            items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'Componentes a consideracion',
+            items: [
+                { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+                { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+                { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
+                { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
+                { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
+                { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
+                { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+                { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+                { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' },
+            ]
+        },
+        {
+            label: 'Paginas',
+            icon: 'pi pi-fw pi-briefcase',
+            to: '/pages',
+            items: [
+                {
+                    label: 'Auth',
+                    icon: 'pi pi-fw pi-user',
+                    items: [
+                        {
+                            label: 'Login',
+                            icon: 'pi pi-fw pi-sign-in',
+                            to: '/auth/login'
+                        },
+                        {
+                            label: 'Access Denied',
+                            icon: 'pi pi-fw pi-lock',
+                            to: '/auth/access'
+                        }
+                    ]
+                },
+                {
+                    label: 'Crud',
+                    icon: 'pi pi-fw pi-pencil',
+                    to: '/pages/crud'
+                },
+                {
+                    label: 'Timeline',
+                    icon: 'pi pi-fw pi-calendar',
+                    to: '/pages/timeline'
+                }
+            ]
+        },
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
