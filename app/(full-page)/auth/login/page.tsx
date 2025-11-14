9046453ce33131079d0a6e816c6bb498a1dcde7e
IsaiasJA <isaiasjuareza04@gmail.com>
2025-11-14 14:01:25 -0600

    Actualizacion de pantalla login

--- app/(full-page)/auth/login/page.tsx
+++ app/(full-page)/auth/login/page.tsx
@@ -2,7 +2,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import React, { useContext, useState } from 'react';
-import { Checkbox } from 'primereact/checkbox';
 import { Button } from 'primereact/button';
 import { Password } from 'primereact/password';
 import { LayoutContext } from '../../../../layout/context/layoutcontext';
